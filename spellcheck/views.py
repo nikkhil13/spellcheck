f3a7683550db645195cde4fbfe94ceed6c8a4f21
nikhil <nikuu13@gmail.com>
2017-04-16 19:09:45 +0530

    some bug fixes

--- spellcheck/views.py
+++ spellcheck/views.py
@@ -35,12 +35,23 @@ def known_edits2(word):
 def known(words): return set(w for w in words if w in NWORDS)
 
 def correct(request):
+	word = ""
+	answer = ""
 	word = request.body
-	#print word
+
 	if word.startswith('"') and word.endswith('"'):
 		word = word[1:-1]
+	word = word.strip()
+	if len(word) < 3:
+		answer = word
+		#answer = answer.strip()
+	else:
 	#print word
-	candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-	answer = max(candidates, key=NWORDS.get)
+		candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+		answer = max(candidates, key=NWORDS.get)
+	answer = answer.strip()
+	print len(answer)
+	print len(word)
+	#print(len(answer))
 	return HttpResponse(answer)
 	
